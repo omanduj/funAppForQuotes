ef272c8d0c0311c338ff9b67c0216a38a0240e18
omanduj <omanduj@gmail.com>
2020-06-07 02:03:29 -0500

    Created an accordian within a product component

--- components/Product.js
+++ components/Product.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { withNavigation } from '@react-navigation/compat';
 import { StyleSheet, Dimensions, Image, TouchableWithoutFeedback } from 'react-native';
-import { Block, Text, theme } from 'galio-framework';
+import { Block, Text, theme, Accordion } from 'galio-framework';
 
 import materialTheme from '../constants/Theme';
 
 const { width } = Dimensions.get('screen');
 
+
+const data = [
+  { title: "\"Title\"", content: "\"Inserted Quote\"",
+    icon: {
+      family: 'material',
+      size: 16,
+    }
+ },
+  { title: "Uriel Smells", content: "He said he smells like a fish" },
+  { title: "Baby Pooped", content: "She went outside and simply pooped" }
+];
+
+
+
 class Product extends React.Component {
+  state = { showAccordian: true };
+
+
+  toggleSwitch = () => {
+    this.setState({ showAccordian: !this.state.showAccordian })
+  }
+
+
   render() {
     const { navigation, product, horizontal, full, style, priceColor, imageStyle } = this.props;
     const imageStyles = [styles.image, full ? styles.fullImage : styles.horizontalImage, imageStyle];
@@ -19,12 +41,15 @@ class Product extends React.Component {
             <Image source={{ uri: product.image }} style={imageStyles} />
           </Block>
         </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback onPress={() => navigation.navigate('NancyMandujano', { product: product })}>
+        <TouchableWithoutFeedback onPress={() => this.toggleSwitch()}>
           <Block flex space="between" style={styles.productDescription}>
+            {this.state.showAccordian ? <Accordion dataArray={data} /> : null}
             <Text size={14} style={styles.productTitle}>{product.title}</Text>
             <Text size={12} muted={!priceColor} color={priceColor}>${product.price}</Text>
           </Block>
         </TouchableWithoutFeedback>
+
+
       </Block>
     );
   }
